ef31aa232d918f42e4e205019a2147f7e448afd4
Derek Hassick <derekhassick@Dereks-MacBook-Air.local>
2018-12-13 16:05:40 -0700

    removed the hello component and moved contents to home

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,20 +1,25 @@
 <template>
-  <div class="home">
-    
-
-    <Hello msg="Welcome"/>
+  <div class="Home">
+    <div class="jumbotron">
+    <h1 class="display-3">Hello! Welcome!</h1>
+    <p class="lead">to the simple gift exchange generator</p>
+      <hr class="my-4">
+        <p>
+          We do not keep your email adresses or names, we just assign a person to eachother 
+          and send them an email of who they have. And of course we send the person 
+          organizing a list of who everyone has, in case they forget or loose their email.
+        </p>
+        <p class="lead">
+          <a class="btn btn-primary btn-lg" href="#" role="button">Lets get started!</a>
+        </p>
+    </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import Hello from '@/components/Hello.vue';
-
 
 export default {
-  name: 'home',
-  components: {
-    Hello
-  },
-};
+  name: 'home'
+  };
 </script>
