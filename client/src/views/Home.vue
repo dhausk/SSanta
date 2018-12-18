1eac685557794477985d1b018f7081ea08fb7fa6
Derek Hassick <derekhassick@Dereks-MacBook-Air.local>
2018-12-18 16:14:44 -0700

    spelling fixes

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -5,9 +5,9 @@
     <p class="lead">to the simple gift exchange generator</p>
       <hr class="my-4">
         <p>
-          We do not keep your email adresses or names, we just assign a person to eachother 
+          We do not sell or give away your email adresses or names, we just assign a person to eachother 
           and send them an email of who they have. And of course we send the person 
-          organizing a list of who everyone has, in case they forget or loose their email.
+          organizing a list of who everyone has, in case someone forgets or looses their email.
         </p>
         <p class="lead">
           <a class="btn btn-primary btn-lg" href="#" role="button">Lets get started!</a>
