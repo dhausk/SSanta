211eb5a2941b439a17ea1658925167440df875b1
Derek Hassick <derekhassick@Dereks-MacBook-Air.local>
2019-04-10 12:19:10 -0600

    added v-for instance for form view.

--- client/src/components/SSList.vue
+++ client/src/components/SSList.vue
@@ -16,16 +16,19 @@
 					</div>
 				</div>
 				<div class="form-group">
-
-
 					<label for="name1">Name</label>
 					<input type="text" class="form-control" id="name1" placeholder="Name">
            <label for="email1">Email address</label>
           <input type="email" class="form-control" id="email1" placeholder="Their email">
-          <label for="name2">Name</label>
-					<input type="text" class="form-control" id="name2" placeholder="Name">
-           <label for="email2">Email address</label>
-          <input type="email" class="form-control" id="email2" placeholder="Their Email">
+          <div v-for="n in listNumber" :key="n">
+
+            <label for="name">Name</label>
+					<input type="text" class="form-control" :id="n" :placeholder="n">
+           <label for="email1">Email address</label>
+          <input type="email" class="form-control" id="email1" placeholder="Their email">
+            
+          </div>
+
 				</div>
           
 				<button type="button" class="btn btn-primary" >Add another person</button>
@@ -36,7 +39,16 @@
 	</div>
 </template>
 <script>
-export default { name: 'SSList'};
+  import {mapState} from 'vuex';
+export default {
+  name: 'SSList',
+  computed: {
+    listNumber () {
+      return this.$store.state.groupSize -1
+    }
+  }
+};
+
 </script>
 
 <style scoped>
