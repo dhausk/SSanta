d3208217b58ae979f8e9df818d9c45dabc3607eb
Derek Hassick <derekhassick@Dereks-MacBook-Air.local>
2019-06-06 19:28:13 -0600

    update npm

--- client/src/views/EmailSignUp.vue
+++ client/src/views/EmailSignUp.vue
@@ -7,7 +7,9 @@
   	</div>
     <form @submit.prevent="createSSGroup">
       <fieldset>
-        <legend>Start by filling out your email and name </legend>
+        <legend>
+          Start by filling out how many people in your group and if you want the results hidden.
+        </legend>
         <div class="form-group row">
           <label for="staticEmail" class="col-sm-2 col-form-label">Email</label>
           <div class="col-sm-10">
@@ -15,20 +17,31 @@
           </div>
         </div>
         <div class="form-group">
-          <label for="creatorName">Your Name</label>
-          <input type="text" v-model="setName" class="form-control" id="creatorName" placeholder="Your Name Here" required>
           
-          <label for="exampleInputEmail1"> Your Email address</label>
-          <input type="email" v-model="setEmail" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email" required>
-          <small id="emailHelp" class="form-text text-muted">We never share your email with anyone outside your group.</small>
-      
-          <label for="number of people">Number of people in your group, including yourself</label>
-          <input type="number" v-model="setGroup" class="form-control" id="number of people" min="3" max="300" placeholder="Number of people" aria-describedby="numberHelp" required>
-          <small id="numberHelp" class="form-text text-muted">You need to have three or more people to play SSanta.</small>
+          <label for="number of people">
+            Number of people in your group, including yourself
+          </label>
+          <input 
+          type="number" 
+          v-model="setGroup" 
+          class="form-control" 
+          id="number of people" 
+          min="3" max="300" 
+          placeholder="Number of people" 
+          aria-describedby="numberHelp" 
+          required>
+          <small id="numberHelp" class="form-text text-muted">
+            You need to have three or more people to play SSanta.
+          </small>
           
           <div class="form-check">
             <label class="form-check-label">
-              <input v-model="setShowHide" class="form-check-input" type="checkbox" value="" checked="">
+              <input 
+              v-model="setShowHide" 
+              class="form-check-input" 
+              type="checkbox" 
+              value="" 
+              checked="">
               Do you want to keep your results hidden?
             </label>
           </div>
@@ -49,22 +62,6 @@
 import {mapMutations, mapActions} from 'vuex';
 export default {
   computed:{
-    setName: {
-      get() {
-        return this.$store.state.creatorName;
-      },
-      set(value){
-        this.$store.commit('setNewName', value);
-      }
-    },
-    setEmail: {
-      get() {
-        return this.$store.state.creatorEmail;
-      },
-      set(value){
-        this.$store.commit('setNewEmail', value);
-      }
-    },
     setGroup: {
       get() {
         return this.$store.state.groupSize;
