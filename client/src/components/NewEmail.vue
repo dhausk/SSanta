36ad40312d753208f77d0b7a14f95cf4272c175c
Derek Hassick <derek.hassick@gmail.com>
2019-07-26 16:31:04 -0600

    added store components to new name 'n email comp

---
+++ client/src/components/NewEmail.vue
@@ -0,0 +1,20 @@
+<template>
+  
+</template>
+<script>
+import { mapActions } from 'vuex';
+export default {
+  setEmail: {
+      get() {
+        return this.$store.state.creatorEmail;
+      },
+      set(value){
+        this.$store.commit('setNewEmail', value);
+      },
+    },
+    name: 'NewEmail',
+}
+</script>
+<style>
+
+</style>
---
+++ client/src/components/NewName.vue
@@ -0,0 +1,24 @@
+<template>
+  
+</template>
+<script>
+export default {
+  computed: {
+    personName: {
+      get() {
+        return this.$store.state.group;
+      },
+      set(value){
+        this.$store.commit('setNewName', value);
+      }  
+    },
+    name: 'NewName',
+  }
+}
+</script>
+<style scoped>
+
+</style>
+
+
+
