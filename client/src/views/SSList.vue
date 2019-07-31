3f52228b97ac002d50b68d4ee8ae1a2661ecd9c4
Derek Hassick <derek.hassick@gmail.com>
2019-07-31 14:08:32 -0600

    added mapfields package, chaged group arr 2 people

--- client/src/views/SSList.vue
+++ client/src/views/SSList.vue
@@ -1,7 +1,11 @@
 <template>
   <div>
     <div class="progress">
-      <div class="progress-bar" role="progressbar" style="width: 50%;" aria-valuenow="25" aria-valuemin="0"
+      <div class="progress-bar"
+        role="progressbar"
+        style="width: 50%;"
+        aria-valuenow="25"
+        aria-valuemin="0"
         aria-valuemax="100">
       </div>
     </div>
@@ -16,36 +20,46 @@
           </div>
         </div>
         <div class="form-group">
-          <div v-for="(item, index) in group" 
-          v-bind:key="item.id"
-          v-bind:index="index"
-          v-bind:item="item"
-          v-bind:group="group">
-            <label for="name">Person {{item.id}}'s name.</label>
-            <input type="text" class="form-control" :id=index  placeholder="Name"> 
-            <label for="email1">Email address for person {{item.id}}</label>
-            <input type="email" class="form-control" :id=index placeholder="Email address">
+          <div v-for="people in peoples"
+          v-bind:key="people.id"
+          v-bind:item="people"
+          v-bind:group="people">
+            <label for="name">Person {{people.id}}'s name.</label>
+            <input type="text" class="form-control" :id=people.id v-model="people.name"
+            placeholder="Name">
+            <label for="email">Email address for person {{people.id}}</label>
+            <input type="email" class="form-control" :id=people.id v-model="people.email"
+            placeholder="Email address">
           </div>
         </div>
-        <button type="button" class="btn btn-primary" >Add another person</button>
+        <button type="button"
+        class="btn btn-primary"
+        v-on:click="addGroupMember">
+        Add another person
+        </button>
+        <button type="button"
+        class="btn btn-primary"
+        v-on:click="deleteGroupMember">
+        delete last person from the list
+        </button>
         <button type="submit" class="btn btn-primary btn-lg btn-block">Next Step</button>
         </fieldset>
     </form>
   </div>
 </template>
 <script>
-import { mapState } from 'vuex';
+import { mapMutations } from 'vuex';
 
-
+import { mapMultiRowFields } from 'vuex-map-fields';
 
 export default {
   name: 'SSList',
   computed: {
-    ...mapState(['group']),
-  }
+    ...mapMultiRowFields(['peoples']),
+  },
+  methods: {
+    ...mapMutations(['addGroupMember', 'deleteGroupMember']),
+  },
 };
 
 </script>
-
-<style scoped>
-</style>
