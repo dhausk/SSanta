6503e957a67780a42f562874daffeacd5ae670b6
Derek Hassick <derek.hassick@gmail.com>
2019-07-31 23:16:34 -0600

    added if operators to selected features and state

--- client/src/views/Confirm.vue
+++ client/src/views/Confirm.vue
@@ -8,13 +8,33 @@
       aria-valuemin="0"
       aria-valuemax="100"> </div>
     </div>
-      <h1>Confirmation page</h1>
+      <h1>Please confirm if all the information is correct.</h1>
+      <section class="container">
+        <h3>There are {{groupSize}} in your Secret Santa group.</h3>
+        <div class="row">
+          <p>The Administrator has elected to</p>
+          <p v-if="showData">not have the master list of pairs.</p>
+          <p v-else >to have a copy of all pairs.</p>
+
+        </div>
+      </section>
+      <section class="container">
+        <div v-for="people in peoples" v-bind:key="people.id" class="card">
+          <h3>{{people.name}}</h3>
+          <p>{{people.email}}</p>
+          <p v-if="people.admin">This person is an Administrator</p>
+        </div>
+      </section>
   </div>
 </template>
 <script>
+import { mapState } from 'vuex';
 
 export default {
   name: 'Confirm',
+  computed: {
+    ...mapState(['peoples', 'groupSize', 'showData']),
+  },
 };
 </script>
 
