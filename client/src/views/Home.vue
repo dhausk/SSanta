aff494e1cd4be6cb007314b5a0e2b6dccc4e8202
Derek Hassick <derek.hassick@gmail.com>
2019-08-01 00:47:15 -0600

    style and grammer and syntax fixes

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,17 +1,19 @@
 <template>
   <div class="Home">
-    <div class="jumbotron">
-    <h1 class="display-3">Hello! Welcome!</h1>
-    <p class="lead">To the simple gift exchange generator</p>
-      <hr class="my-4">
-        <p>
-          We do not sell or give away your email adresses or names, we just assign a person to eachother
+    <div class="jumbotron text-center" >
+      <h1 class="display-3">Hello!  And Welcome!</h1>
+      <p class="lead">To the simple gift exchange generator</p>
+      <hr>
+        <p class="text-justify p-4 m-4" >
+          We do not sell or give away your email adresses or names, we just
+          assign a person to eachother
           and send them an email of who they have. And of course we send the person
           organizing a list of who everyone has, in case someone forgets or looses their email.
         </p>
         <p class="lead">
           <router-link to="/SSForm/EmailSignUp" class="nav-link">
-            <button class="btn btn-primary btn-lg btn-block" role="button">Lets get started!</button>
+            <button class="btn btn-primary btn-lg btn-block"
+            role="button">Lets get started!</button>
           </router-link>
         </p>
     </div>
