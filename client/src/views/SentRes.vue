541cb8d08d5512ae6c1a56ecfa2cd2d1d698fc67
Derek Hassick <derek.hassick@gmail.com>
2019-08-01 00:49:25 -0600

    created sent res vue comp

---
+++ client/src/views/SentRes.vue
@@ -0,0 +1,19 @@
+<template >
+  <div>
+    <div class="progress mb-4">
+      <div class="progress-bar"
+      role="progressbar"
+      style="width: 100%;"
+      aria-valuenow="25"
+      aria-valuemin="0"
+      aria-valuemax="100"> </div>
+    </div>
+  </div>
+</template>
+<script>
+import { mapState } from 'vuex';
+
+export default {
+  name: 'SentResponse',
+};
+</script>
