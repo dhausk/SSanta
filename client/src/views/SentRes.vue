35bc3d5108f452e13beff8af513ace96c861d91d
Derek Hassick <derek.hassick@gmail.com>
2019-08-04 14:30:27 -0600

    added reponse header

--- client/src/views/SentRes.vue
+++ client/src/views/SentRes.vue
@@ -1,19 +1,28 @@
 <template >
   <div>
-    <div class="progress mb-4">
-      <div class="progress-bar"
-      role="progressbar"
-      style="width: 100%;"
-      aria-valuenow="25"
-      aria-valuemin="0"
-      aria-valuemax="100"> </div>
+    <div class="container">
+      <div class="progress mb-4">
+        <div class="progress-bar"
+        role="progressbar"
+        style="width: 100%;"
+        aria-valuenow="25"
+        aria-valuemin="0"
+        aria-valuemax="100"> </div>
+      </div>
     </div>
+    <section>
+      <h2>Your gift exchange emails have been sent.</h2>
+      <!-- need to have message response from server side showing all succesful emails and
+      form validations
+      once cleared have the state cleared and ready for the next batch. -->
+    </section>
   </div>
 </template>
 <script>
-import { mapState } from 'vuex';
-
 export default {
-  name: 'SentResponse',
+  name: 'SentRes',
+  // async mounted() {
+
+  // },
 };
 </script>
