49e3ed5f8318af7d98467c3f7030e35e51432d73
Derek Hassick <derek.hassick@gmail.com>
2019-08-13 12:16:11 -0600

    valid middle ware is being used

--- server/app.js
+++ server/app.js
@@ -3,26 +3,27 @@ const morgan = require("morgan");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const app = express();
-// const valid = require("./validate");
+const valid = require("./validate");
 
 app.disable('x-powered-by')
 
 app.use(morgan('dev'));
 app.use(cors());
-// app.use(bodyParser.json());
+app.use(bodyParser.json());
 
 app.get("/", (req, res) => {
   res.status(200).json({ message: "welcome to the SSanta Gift exchange server."});
 });
 
-// app.use(valid());
-// app.post('/', (req, res, next) => {
-//   if (req.valid) {
-//       res.json({message: 'good job, its valid.'})
-//   } else {
-//     next(new Error('form not valid. please double check your form.'))
-//   }
-// })
+app.use(valid());
+
+app.post('/form', (req, res, next) => {
+  if (req.valid) {
+      res.json({message: 'good job, its valid.'})
+  } else {
+    next(new Error('form not valid. please double check your form.'))
+  }
+})
 
 app.use(function (err, req, res, next) {
   // render the error page
