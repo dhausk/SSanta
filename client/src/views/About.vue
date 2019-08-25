9fde859001897bc5c30c962b77ffe0ac9309348f
Derek Hassick <derek.hassick@gmail.com>
2019-08-25 16:06:02 -0600

    added new icons and logo.

--- client/src/views/About.vue
+++ client/src/views/About.vue
@@ -5,13 +5,13 @@
     <p>Before the Christmas of 2018, I was looking for a Secret Santa Generator
       and each one I found wanted me to create an account. Once you created an
       each person in your list had to sign up for a group and make an account.
-      Then once everyone signed up, It made you draw names together. 
+      Then once everyone signed up, It made you draw names together.
     </p>
     <p>As you can tell that was too much work, I just wanted to send a few emails with a
       persons Santa and giftee. Plus Im pretty sure they sold my info fund their service.
     </p>
     <p>So I decided to create a my own Secret Santa. One that just took in a bunch names,
-       randomily paired everyone up and just send out who you have. No registrations, 
+       randomily paired everyone up and just send out who you have. No registrations,
        no creepy face book ads. Simple and right to the point. Have a great Gift Exchange!
     </p>
     </section>
